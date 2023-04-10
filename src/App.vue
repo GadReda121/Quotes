cac1f99aeea873359de915a78f8d3779f5a3d854
GadReda121 <khtabjab@gmail.com>
2023-04-10 03:41:17 +0200

    Almost Done

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,85 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="parent">
+    <!-- Quotes -->
+    <div class="quotes">
+        <div class="quote">
+            <font-awesome-icon icon="fa-solid fa-quote-left"/>
+            <font-awesome-icon icon="fa-solid fa-quote-right" class="mb-3"/>
+            <p v-for="quote in quotes" :key="quote.id" class="mt-2">{{ quote.content }}</p>
+        </div>
+        <h6 v-for="quote in quotes" :key="quote.id">{{ quote.author }}</h6>
+        <small v-for="quote in quotes" :key="quote.id">{{ quote.dateAdded }}</small>
+        <!-- Share -->
+        <div class="share d-flex">
+          <!-- social -->
+          <div class="social">
+            <!-- Copy -->
+            <div class="copy">
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+            </div>
+            <!-- twitter -->
+          </div>
+          <button type="button" @click="generate">Quote</button>
+        </div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      quotes: []
+    }
+  },
+  mounted() {
+    this.generate();
+  },
+  methods: {
+    generate() {
+      axios.get("http://api.quotable.io/random")
+        .then((data => this.quotes = data))
+    }
+ }
+}
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
+.quotes {
+  background-color: #fff;
+  border-radius: 8px; 
+  box-shadow: 0px 0px 100px rgba(0,0,0,.1);
+  padding: 20px;
+  align-self: center;
+  text-align: center;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+small{
+  font-size: 10px;
 }
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+p{
+  font-weight: bolder;
+}
+h6{
+  font-weight: lighter;
+}
+.parent{
+  width:40%;
+  height: 80vh;
+  margin:auto;
+  display: grid;
+  grid-template-rows: minmax(100px,1fr);
+}
+@media screen and (max-width:767px){
+  .parent{
+    width:80%;
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+}
+button{
+  padding: 10px;
+  border-radius: 5px;
+  background-color: #000;
+  color:#fff;
+  flex:1;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
 import './assets/main.css'
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+/* import specific icons */
+import { faQuoteLeft, faQuoteRight } from '@fortawesome/free-solid-svg-icons'
+library.add(faQuoteLeft, faQuoteRight);
 
-createApp(App).mount('#app')
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
