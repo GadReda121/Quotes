ccdaa11ba20f590e730cdaa41d21899468469bdf
GadReda121 <khtabjab@gmail.com>
2023-04-11 02:15:42 +0200

    Almost Done

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,14 @@
   <div class="parent">
     <!-- Quotes -->
     <div class="quotes">
+        <!-- Icon & Content -->
         <div class="quote">
-            <font-awesome-icon icon="fa-solid fa-quote-left"/>
-            <font-awesome-icon icon="fa-solid fa-quote-right" class="mb-3"/>
-            <h3 v-for="quote in quotes" :key="quote.id" class="mt-2">{{ quote.content }}</h3>
+            <font-awesome-icon icon="fa-solid fa-quote-right" class="quoteIcon" />
+            <h3 v-for="quote in quotes" :key="quote.id" class="mt-3">{{ quote.content }}</h3>
         </div>
+        <!-- Author Name -->
         <h6 v-for="quote in quotes" :key="quote.id">{{ quote.author }}</h6>
+        <!-- Date -->
         <small v-for="quote in quotes" :key="quote.id">{{ quote.dateAdded }}</small>
         <!-- Share -->
         <div class="share d-flex justify-content-evenly mt-4">
@@ -16,18 +18,11 @@
           <div class="social d-flex align-items-center gap-3">
             <!-- Copy -->
             <div class="copy">
-                <font-awesome-icon icon="fa-solid fa-clipboard" class="icon copy" style="font-size:25px" @click="copy"/>
-            </div>
-            <!-- twitter -->
-            <div class="twitter">
-                <font-awesome-icon icon="fa-brands fa-twitter" class="icon twitter" style="font-size:25px" @click="share"/>
-            </div>
-            <!-- facebook -->
-            <div class="facebook">
-                <font-awesome-icon icon="fa-brands fa-facebook" class="icon facebook" style="font-size:25px" @click="shareOnFacebook"/>
+                <font-awesome-icon icon="fa-solid fa-clipboard" class="icon copy" style="font-size:25px" @click="copy" title="Clip To Clipboard" />
             </div>
           </div>
-          <button type="button" @click="generate">Quote</button>
+          <!-- BTN => Quote -->
+          <button type="button" @click="generate">{{ Quote }}</button>
         </div>
     </div>
   </div>
@@ -42,7 +37,8 @@ export default {
   components: { Preloader },
   data() {
     return {
-      quotes: []
+      quotes: [],
+      Quote : "Quote"
     }
   },
   mounted() {
@@ -52,9 +48,6 @@ export default {
     generate() {
       axios.get("http://api.quotable.io/random")
         .then((data => this.quotes = data))
-    },
-    shareOnFacebook() {
-
     },
     copy() {
       let h3 = document.querySelector("h3");
@@ -87,6 +80,17 @@ export default {
   padding: 20px;
   align-self: center;
   text-align: center;
+  position: relative;
+}
+.quotes::before{
+  content: '';
+  background-color: #000;
+  width: 5px;
+  height:100%;
+  position: absolute;
+  left:0;
+  top:0;
+  border-radius: 8px 0px 0px 8px;
 }
 small{
   font-size: 10px;
@@ -108,6 +112,11 @@ h6{
     width:80%;
   }
 }
+@media screen and (min-width:768px) and (max-width:991px){
+  .parent{
+    width:60%;
+  }
+}
 button{
   padding: 10px;
   border-radius: 5px;
@@ -115,15 +124,19 @@ button{
   color:#fff;
   flex: .5;
 }
+.quoteIcon{
+  font-size: 40px;
+  color: #fff;
+  background-color: #000;
+  padding: 20px 23px;
+  border-radius: 50px;
+  margin-top: -100px;
+  box-shadow: 0px -5px 100px rgba(0,0,0,.1);
+  position: relative;
+}
 .icon{
   cursor: pointer;
   transition: all .3s;
   color:#000;
-}
-.facebook:hover{
-  color:#1877f2
-}
-.twitter:hover{
-  color:#1DA1F2
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,18 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+// Bootstrap
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
-import './assets/main.css'
+// Global CSS
+import './assets/main.css';
 /* import the fontawesome core */
-import { library } from '@fortawesome/fontawesome-svg-core'
+import { library } from '@fortawesome/fontawesome-svg-core';
 /* import font awesome icon component */
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 /* import specific icons */
 import { faQuoteLeft, faQuoteRight, faClipboard } from '@fortawesome/free-solid-svg-icons';
 import { faTwitter, faFacebook } from "@fortawesome/free-brands-svg-icons";
+// Library
 library.add(faQuoteLeft, faQuoteRight, faTwitter, faFacebook, faClipboard);
-createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
+// App
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app');
